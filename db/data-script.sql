e637593ed4dd1df0265b2ebb2126025ed9b91223
BuuHQ <BuuHQ@tpb.vn>
2023-07-31 18:19:15 +0700

    commit 6

--- db/data-script.sql
+++ db/data-script.sql
@@ -20,24 +20,36 @@ update song_attributes set band_sheet = 'Careless Whisper F#m.pdf' where tone = 
 
 ----
 insert into songs (name, normalize_name, genre, composer, singer, lyric_url, is_practiced_this_week, ref_link, position) 
-values ('Careless Whisper', 'careless_whisper','pop', 'George Michael', 'George Michael', 
-'Careless Whisper Lyric.docx', 1, 'https://youtu.be/izGwDsrQ1eQ',1);
+values ('Careless Whisper', 'careless-whisper','pop', 'George Michael', 'George Michael', 
+'Careless-Whisper-Lyric.docx', 1, 'https://youtu.be/izGwDsrQ1eQ',1);
 insert into song_attributes (song_id, tone, band_sheet) values
-((select id from songs where normalize_name = 'careless_whisper'), 
-'Fm', 'Careless Whisper Fm.pdf');
+((select id from songs where normalize_name = 'careless-whisper'), 
+'Fm', 'Careless-Whisper-Fm.pdf');
 insert into song_attributes (song_id, tone, band_sheet) values
-((select id from songs where normalize_name = 'careless_whisper'), 
-'F#m', 'Careless Whisper F#m.pdf');
+((select id from songs where normalize_name = 'careless-whisper'), 
+'F#m', 'Careless-Whisper-F#m.pdf');
 insert into song_attributes (song_id, tone, band_sheet) values
-((select id from songs where normalize_name = 'careless_whisper'), 
-'Dm', 'Careless Whisper Dm.pdf');
+((select id from songs where normalize_name = 'careless-whisper'), 
+'Dm', 'Careless-Whisper-Dm.pdf');
 
 insert into songs (name, normalize_name, genre, composer, singer, lyric_url, is_practiced_this_week, ref_link, position) 
 values ('Casablanca', 'casablanca','pop', 'Bertie Higgins', 'Ennah', 
 'Casablanca.docx', 1, 'https://youtu.be/fYqmy_L7eSE', 2);
 insert into song_attributes (song_id, tone, band_sheet) values
 ((select id from songs where normalize_name = 'casablanca'), 
-'Dm', 'Casablanca Em.pdf');
+'Em', 'Casablanca-Em.pdf');
 
 
+insert into songs (name, normalize_name, genre, composer, singer, lyric_url, is_practiced_this_week, ref_link, position) 
+values ('Dont Speak', 'dont-speak','rock', 'No Doubt', 'No Doubt', 
+'Dont-Speak.docx', 1, 'https://youtu.be/TR3Vdo5etCQ', 3);
+insert into song_attributes (song_id, tone, band_sheet) values
+((select id from songs where normalize_name = 'dont-speak'), 
+'Cm', 'Dont-Speak-Cm.pdf');
 
+insert into songs (name, normalize_name, genre, composer, singer, lyric_url, is_practiced_this_week, ref_link, position) 
+values ('Giữa Đại Lộ Đông Tây', 'giua-dai-lo-dong-tay','jazz', 'Hứa Kim Tuyền', 'Uyên Linh', 
+'Giua-Dai-Lo-Dong-Tay.docx', 1, 'https://youtu.be/2AfaQ6qsLbw', 4);
+insert into song_attributes (song_id, tone, band_sheet) values
+((select id from songs where normalize_name = 'giua-dai-lo-dong-tay'), 
+'Bm', 'Giua-Dai-Lo-Dong-Tay-Bm.pdf');
